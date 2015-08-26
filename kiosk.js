707e53745d064e9079f3397c044af2672d10f735
Aubrey Cottle <acottle@taimalabs.com>
2015-08-26 18:10:35 -0400

    we got svgs in this shit

--- kiosk.js
+++ kiosk.js
@@ -59,17 +59,20 @@ Router.route('/', {
 if (Meteor.isClient) {
   // Initial factoid
   Template.factoid.created = function(){
-    Session.setDefault('currentFactoid', 'fact1');
+    Session.setDefault('currentFactoid', rollDice());
+    Session.setDefault('currentAnimation', 'anim1');
   };
 
   var rollDice = function() {
     fact = Math.floor((Math.random() * 14) + 1);
+    // fact = Math.floor((Math.random() * 3) + 1);
     factName = 'fact'+fact;
     return factName;
   }
 
   // Select a random factoid
   var randomFact = function() {
+
     factName = rollDice();
 
     // make sure it doesn't repeat the previous fact
@@ -77,15 +80,43 @@ if (Meteor.isClient) {
       factName = rollDice();
     }
 
+    animation = 'anim'+Math.floor((Math.random() * 5) + 1);
+
+    while(Session.get('currentAnimation') === animation) {
+      animation = 'anim'+Math.floor((Math.random() * 5) + 1);
+    }
+
     Session.set('currentFactoid', factName);
+    Session.set('currentAnimation', animation);
+    // Meteor.setTimeout(function(){
+    //   Session.set('currentAnimation', animation);
+    // },4000);
   }
 
-  var interval = Meteor.setInterval(randomFact, 15000);
+  var randomTitleAnim = function() {
+    animations = Array('bounce','tada','rubberBand','swing','jello','wobble','flip');
+    titleAnimation = animations[Math.floor(Math.random()*animations.length)];
+    Session.set('dykAnimation', 'animated '+titleAnimation);
+  }
+
+  var randomFactInterval = Meteor.setInterval(randomFact, 12000);
+  var randomTitleAnimInterval = Meteor.setInterval(randomTitleAnim, 12000);
+  // var randomAnimationInterval = Meteor.setInterval(randomAnimation, 1000);
+
+  Template.registerHelper('currentFactoid', function(){
+    return Session.get('currentFactoid');
+  });
+
+  Template.registerHelper('currentAnimation', function(){
+    return Session.get('currentAnimation');
+  });
+
+  Template.registerHelper('dykAnimation', function(){
+    return Session.get('dykAnimation');
+  });
 
   Template.factoid.helpers({
-    currentFactoid: function() {
-      return Session.get('currentFactoid');
-    }
+
   });
 
   Template.factoid.events({
@@ -96,6 +127,7 @@ if (Meteor.isClient) {
     //   Session.set('counter', Session.get('counter') + 1);
     // }
   });
+
 
 }
 
