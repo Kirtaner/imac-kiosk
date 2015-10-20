2b397e06247c36b3b3156cbba9465f3b6d97d1a8
Aubrey Cottle <acottle@taimalabs.com>
2015-10-20 18:51:27 -0400

    fact frame fixes

--- factoid.js
+++ factoid.js
@@ -1,65 +1,6 @@
 /* FACTOIDS */
 
 if (Meteor.isClient) {
-  // Initial factoid
-  // Template.factoid.created = function(){
-  //   Session.setDefault('currentFactoid', rollDice());
-  //   Session.setDefault('currentAnimation', 'anim1');
-  // };
-
-
-  var rollDice = function() {
-    fact = Math.floor((Math.random() * 14) + 1);
-    // fact = Math.floor((Math.random() * 3) + 1);
-    factName = 'fact'+fact;
-    return factName;
-  }
-
-  // Select a random factoid
-  var randomFact = function() {
-
-    factName = rollDice();
-
-    // make sure it doesn't repeat the previous fact
-    while(Session.get('currentFactoid') === factName) {
-      factName = rollDice();
-    }
-
-    animation = 'anim'+Math.floor((Math.random() * 5) + 1);
-
-    while(Session.get('currentAnimation') === animation) {
-      animation = 'anim'+Math.floor((Math.random() * 5) + 1);
-    }
-
-    Session.set('currentFactoid', factName);
-    Session.set('currentAnimation', animation);
-    // Meteor.setTimeout(function(){
-    //   Session.set('currentAnimation', animation);
-    // },4000);
-  }
-
-  var randomTitleAnim = function() {
-    animations = Array('bounce','tada','rubberBand','swing','jello','wobble','flip');
-    titleAnimation = animations[Math.floor(Math.random()*animations.length)];
-    Session.set('dykAnimation', 'animated '+titleAnimation);
-  }
-
-  var randomFactInterval = Meteor.setInterval(randomFact, 4000);
-  var randomTitleAnimInterval = Meteor.setInterval(randomTitleAnim, 4000);
-
-  Template.registerHelper('currentFactoid', function(){
-    return Session.get('currentFactoid');
-  });
-
-  Template.registerHelper('currentAnimation', function(){
-    return Session.get('currentAnimation');
-  });
-
-  Template.registerHelper('dykAnimation', function(){
-    return Session.get('dykAnimation');
-  });
-
-
 
   /* Kiosk frame helpers */
 
@@ -76,18 +17,48 @@ if (Meteor.isClient) {
     return Session.get('currentBackground');
   });
 
-  // var randomTitleAnimInterval = Meteor.setInterval(randomTitleAnim, 4000);
-
-  /* TTS Callout helpers */
+  /* TTS Callout frame helpers */
 
   var randomTTSCallout = function() {
-    frames = Array('consoles','samsungServiceCentre');
+    frames = Array('consoles','samsungServiceCentre','apple');
     frame = frames[Math.floor(Math.random()*frames.length)];
+    return frame;
+  }
+
+  var randomTTSCalloutBusiness = function() {
+    frame = 'businessQuip'+Math.floor((Math.random() * 2) + 1);
     return frame;
   }
 
   Template.registerHelper('ttsCalloutRandom', function(){
     return randomTTSCallout();
+  });
+
+  Template.registerHelper('ttsCalloutBusinessRandom', function(){
+    return randomTTSCalloutBusiness();
+  });
+
+  /* Quote frame helpers */
+
+  var randomQuote = function() {
+    quote = 'quote'+Math.floor((Math.random() * 13) + 1);
+    return quote;
+  }
+
+  Template.registerHelper('currentQuote', function(){
+    return randomQuote();
+  });
+
+  /* Factoid helpers */
+
+  var randomFact = function() {
+    fact = Math.floor((Math.random() * 14) + 1);
+    factName = 'fact'+fact;
+    return factName;
+  }
+
+  Template.registerHelper('currentFactoid', function(){
+    return randomFact();
   });
 
 }
