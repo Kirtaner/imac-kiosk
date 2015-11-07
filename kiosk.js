35411c5d374fd5a6d2b419240b4207f48adef108
Kirtaner <acottle@taimalabs.com>
2015-11-07 12:02:41 -0500

    spinner

--- kiosk.js
+++ kiosk.js
@@ -218,5 +218,24 @@ if (Meteor.isClient) {
     idleModal = setTimeout(function(){ Modal.hide(); }, 30000);
   }
 
+  Meteor.Spinner.options = {
+      lines: 13, // The number of lines to draw
+      length: 10, // The length of each line
+      width: 5, // The line thickness
+      radius: 15, // The radius of the inner circle
+      corners: 0.7, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#fff', // #rgb or #rrggbb
+      speed: 1, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: true, // Whether to render a shadow
+      hwaccel: true, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: '400px', // Top position relative to parent in px
+      left: '600px', // Left position relative to parent in px
+  };
+
 };
 
